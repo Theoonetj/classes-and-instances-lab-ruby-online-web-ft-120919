54d1abb946721f2dd3c589e1c381dc77c2e71f36
confident-token-1019 <theoonetj@gmail.com>
2019-12-21 14:37:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,11 @@
-class Dog 
-  
+class Dog
 end
-  fido = Dog.new 
-  snoppy = Dog.new
-  lassie = Dog.new 
+ 
+fido = Dog.new
+fido #=> #<Dog:0x007fc52c2d7d20>
+ 
+snoopy = Dog.new
+snoopy #=> #<Dog:0x007fc52c2d4170>
+ 
+lassie = Dog.new
+lassie #
